dc600c4ee56e1159437a1f3613f4fc0ec6df507d
jdetok <jdeko17@gmail.com>
2025-07-05 17:21:31 -0500

    sql

--- internal/mariadb/test/test.go
+++ internal/mariadb/test/test.go
@@ -1,28 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 
 	"github.com/jdetok/go-api-jdeko.me/internal/mariadb"
 )
 
-
 func main() {
 	db := mariadb.InitDB()
-	q := `
-		select * from v_szn_avgs
-		where season = ?
-		and team = ?
-	`
-	resp, err := mariadb.DBJSONResposne(db, q, "2016-2017 Regular Season", "LAL")
+	// q := `
+	// 	select * from v_szn_avgs
+	// 	where season = ?
+	// 	and team = ?
+	// `
+	resp, err := mariadb.DBJSONResposne(db, "select * from season_totals")
 	if err != nil {
 		fmt.Printf("Error occured querying db: %v\n", err)
 	}
-	
-	// fmt.Println(string(resp))
 
-	var data any
-	json.Unmarshal(resp, &data)
-	fmt.Println(data)
+	fmt.Println(string(resp))
+
+	// var data any
+	// json.Unmarshal(resp, &data)
+	// fmt.Println(data)
 }
