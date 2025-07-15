feec7b45910791d3d3a7134e9bf1f1dd1e6f1688
jdetok <jdeko17@gmail.com>
2025-07-14 21:53:51 -0500

    devl

--- static/bball/js/listen.js
+++ static/bball/js/listen.js
@@ -8,11 +8,14 @@ import * as selectors from "./selectors.js"
 export const base = "https://jdeko.me/bball";
 // export const dev = "https://jdeko.me/devl/bball";
 
-export let crnt = "first";
-export async function updateCrnt(new_crnt) {
-    console.log(`current pre test: ${crnt}`)
-    crnt = new_crnt;
-    console.log(`current post test: ${crnt}`);
+export async function getRecGames() {
+    const r = await fetch(`${base}/games/recent`);
+    if (!r.ok) {
+        throw new Error(`${r.status}: error calling /games/recent`);
+    }
+    const data = await r.json();
+    const player = data.top_scorers[0].player_id;
+    await pdash.getP(base, player, 88888, 0, data);
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -22,11 +25,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     await buttons.search();
     await buttons.clearSearch();
     await buttons.holdPlayerBtn();
-    await pdash.getP(base, 'random', 88888, 0);
+    // await pdash.getP(base, 'random', 88888, 0);
+
+    await getRecGames();
 });
-// await home.loadSeasonOpts();
-// await home.loadTeamOpts();
-    // await home.gamesRecent();
-    // await home.topScorer();
-    // await home.lgChangeListener();
-    // await buttons.clear();
