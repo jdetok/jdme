61be64a69478d98f4b1a7f49ed5faeeafd44f0a7
jdetok <jdeko17@gmail.com>
2025-08-06 18:09:27 -0500

    before running migrate script

---
+++ scripts/pg_migrate.sh
@@ -0,0 +1,19 @@
+# migrate to pg - copy dirs from dev-jdeko.me
+GODIR=/home/jdeto/go/github.com/jdetok
+PROD=$GODIR/go-api-jdeko.me
+DEV=$GODIR/dev-jdeko.me
+
+cd $PROD
+
+cp -r $DEV/pgdb pgdb
+go get github.com/jdetok/golib
+cp -r $DEV/api $PROD/api
+
+cp $DEV/.air.toml .dev.air.toml # verify & update manually
+cp $DEV/.env dev.env # verify & update manually
+
+rm -r getenv
+rm -r applog
+rm -r .vscode
+rm -r mdb 
+go mod tidy
