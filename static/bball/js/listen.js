6aac5e7c30716a53d8a8c59c93e4bfe884a557dd
jdetok <jdeko17@gmail.com>
2025-09-19 12:13:42 -0500

    ready to merge in leading scorers update

--- static/bball/js/listen.js
+++ static/bball/js/listen.js
@@ -1,18 +1,18 @@
 // script to load in HTML -- all listener functions are called here 
-import * as ui from "./ui.js"
-import * as resp from "./resp.js"
+import { loadSznOptions, selHvr, randPlayerBtn, search, clearSearch, 
+    holdPlayerBtn } from "./ui.js"
 import { makeScoringLeaders } from "./lg_ldg_scorers.js"
 import { makeRGTopScorers } from "./rg_ldg_scorers.js";
 
 export const base = "http://localhost:8080/bball";
 
 document.addEventListener('DOMContentLoaded', async () => {
-    await ui.loadSznOptions();
-    await ui.selHvr();
-    await ui.randPlayerBtn();
-    await ui.search();
-    await ui.clearSearch();
-    await ui.holdPlayerBtn();
+    await loadSznOptions();
+    await selHvr();
+    await randPlayerBtn();
+    await search();
+    await clearSearch();
+    await holdPlayerBtn();
     await makeRGTopScorers();
     await makeScoringLeaders(10);
 });
