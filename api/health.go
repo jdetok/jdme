d41322602e390f097ddc915b15dc920702dfebbe
jdetok <jdeko17@gmail.com>
2025-12-10 21:32:36 -0600

    added recent game query to health endpoint

--- api/health.go
+++ api/health.go
@@ -2,17 +2,20 @@ package api
 
 import (
 	"net/http"
-	"time"
 )
 
 func (app *App) HndlHealth(w http.ResponseWriter, r *http.Request) {
-	if time.Since(app.LastUpdate) > 3*time.Hour {
-		http.Error(w, "store not updated", 500)
-		return
-	}
+	app.Lg.LogHTTP(r)
 	if err := app.DB.Ping(); err != nil {
 		http.Error(w, "db down", 500)
 		return
 	}
+	var recGameDate string
+	row := app.DB.QueryRow(`select gdate from stats.pbox order by gdate desc limit 1`)
+	if err := row.Scan(&recGameDate); err != nil {
+		http.Error(w, "db ping successful and query returned, failed to scan row to string", 500)
+		return
+	}
 	w.WriteHeader(200)
+	w.Write([]byte("jdeko.me is healthy :)\nmost recent game date in db: " + recGameDate))
 }
