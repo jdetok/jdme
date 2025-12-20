f0fb82ba323802a26f7b25ce18704201203bce37
jdetok <jdeko17@gmail.com>
2025-12-19 19:27:04 -0600

    multiple patterns

--- scripts/ci/localhost_to_prod.py
+++ scripts/ci/localhost_to_prod.py
@@ -3,7 +3,11 @@
 import pathlib
 import re
 
-local_ptrn = re.compile(r"http://localhost:[0-9]+")
+# local_ptrn = re.compile(r"http://localhost:[0-9]+")
+patterns = [r"https?://localhost:[0-9]+",
+            r"https?://jdeko.me[:\/]?[0-9]*"
+            ]
+
 prod_url = "https://jdeko.me"
 
 num_replaced = 0
@@ -12,12 +16,14 @@ try:
         print(f"in {path}")
         if path.suffix in [".js", ".html", ".css", ".go"]:
             try:
-                txt = path.read_text()
-                new_txt = local_ptrn.sub(prod_url, txt)
-                if txt != new_txt:
-                    path.write_text(new_txt)
-                    num_replaced += 1
-                    print(f"replaced match to {local_ptrn}in {path}")
+                for ptrn in patterns:
+                    txt = path.read_text()
+                    # new_txt = local_ptrn.sub(prod_url, txt)
+                    new_txt = re.compile(ptrn).sub(prod_url, txt)
+                    if txt != new_txt:
+                        path.write_text(new_txt)
+                        num_replaced += 1
+                        print(f"replaced match to {ptrn} in {path}")
             except Exception as e:
                 print(f"error occured reading {path}: {e}")
                 raise SystemExit
