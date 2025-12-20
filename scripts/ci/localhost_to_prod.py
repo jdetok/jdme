fb63202cbf899651f0a288c491f881eaa350382a
jdetok <jdeko17@gmail.com>
2025-12-19 19:08:03 -0600

    update py replace script 3

--- scripts/ci/localhost_to_prod.py
+++ scripts/ci/localhost_to_prod.py
@@ -7,16 +7,19 @@ local_ptrn = re.compile(r"http://localhost:[0-9]+")
 prod_url = "https://jdeko.me"
 
 num_replaced = 0
-
-for path in pathlib.Path(".").rglob('*'):
-    if path.suffix in [".js", ".html", ".css", ".go"]:
-        try:
-            txt = path.read_text()
-            new_txt = local_ptrn.sub(prod_url, txt)
-            if txt != new_txt:
-                path.write_text(new_txt)
-                num_replaced += 1
-                print(f"replaced {txt} with {new_txt} in {path}")
-        except: raise
-        finally: print(f"{num_replaced} strings replaced")
+try: 
+    for path in pathlib.Path(".").rglob('*'):
+        if path.suffix in [".js", ".html", ".css", ".go"]:
+            try:
+                txt = path.read_text()
+                new_txt = local_ptrn.sub(prod_url, txt)
+                if txt != new_txt:
+                    path.write_text(new_txt)
+                    num_replaced += 1
+                    print(f"replaced {txt} with {new_txt} in {path}")
+            except: raise SystemExit
+            finally: print(f"error occured reading {path}")
+except: raise SystemExit
+finally: print(f"{num_replaced} strings replaced")
+    
             
